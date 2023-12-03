6afe46958bfa3a9913333372f088d2c75f058bb3
collinsln <collangat@gmail.com>
2023-12-03 15:39:22 +0300

    complete, passed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, anagrams):
+        sorted_word = sorted(self.word.lower())
+
+        find_anagrams = [a for a in anagrams if sorted(a.lower()) == sorted_word and a.lower() != self.word.lower()]
+
+        return find_anagrams
